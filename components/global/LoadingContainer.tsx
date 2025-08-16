16134f7d0673e7ebdba35a7fe35a1a2d8fd6cc62
allan <allanrey.macasaquit@gmail.com>
2025-08-15 23:11:41 -0400

    update loading

--- components/global/LoadingContainer.tsx
+++ components/global/LoadingContainer.tsx
@@ -1,25 +1,32 @@
 import { Skeleton } from '../ui/skeleton';
 import { Card, CardContent } from '../ui/card';
 
-function LoadingContainer() {
+interface LoadingContainerProps {
+	count?: number;
+}
+
+function LoadingContainer({ count = 6 }: LoadingContainerProps) {
+	const placeholders = Array.from({ length: count });
+
 	return (
-		<div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
-			<LoadingProduct />
-			<LoadingProduct />
-			<LoadingProduct />
+		<div className='pt-12 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+			{placeholders.map((_, i) => (
+				<LoadingProduct key={i} />
+			))}
 		</div>
 	);
 }
 
 function LoadingProduct() {
 	return (
-		<Card>
+		<Card className='animate-pulse'>
 			<CardContent className='p-4'>
-				<Skeleton className='h-48 w-full' />
-				<Skeleton className='h-4 w-3/4 mt-4' />
-				<Skeleton className='h-4 w-1/4 mt-4' />
+				<Skeleton className='h-48 w-full rounded-md' />
+				<Skeleton className='h-4 w-3/4 mt-4 rounded' />
+				<Skeleton className='h-4 w-1/4 mt-2 rounded' />
 			</CardContent>
 		</Card>
 	);
 }
+
 export default LoadingContainer;
